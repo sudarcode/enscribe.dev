e2fe32495b47e2548230ee44306e2ef12223b4a0
Sudar Blogger <terateinka@gmail.com>
2026-05-22 10:30:24 +0700

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -60,48 +60,13 @@ export default defineConfig({
   },
   redirects: {
     // v1.0.0 -> v3.0.0
-    '/ctfs/pico22/beginners-compilation':
-      '/blog/picoctf-2022-beginners-compilation',
+    '/post/penanggungan-mojokerto':
+      '/blog/gunung-penanggungan',
     '/ctfs/pico/beginners-compilation':
       '/blog/picoctf-2022-beginners-compilation',
     '/ctfs/pico22/crypto/basic-mod1':
       '/blog/picoctf-2022-beginners-compilation',
-    '/ctfs/pico22/crypto/basic-mod1-2':
-      '/blog/picoctf-2022-beginners-compilation',
-    '/ctfs/pico22/pwn/basic-file-exploit':
-      '/blog/picoctf-2022-beginners-compilation',
-    '/ctfs/pico22/crypto/pwn/cve-xxxx-xxxx':
-      '/blog/picoctf-2022-beginners-compilation',
-    '/ctfs/pico22/pwn/cve-xxxx-xxxx':
-      '/blog/picoctf-2022-beginners-compilation',
-    '/ctfs/pico22/pwn/ropfu': '/blog/picoctf-2022-beginners-compilation',
-    '/ctfs/byu/osint/osint-compilation': '/blog/byuctf-2022-osint-compilation',
-    '/ctfs/utc/prog/port-authority': '/blog/dhhutc-2022-port-authority',
-    '/ctfs/pico22/pwn/buffer-overflow-series':
-      '/blog/picoctf-2022-buffer-overflow',
-    '/ctfs/pico22/pwn/buffer-overflow-0': '/blog/picoctf-2022-buffer-overflow',
-    '/ctfs/pico22/pwn/buffer-overflow-1': '/blog/picoctf-2022-buffer-overflow',
-    '/ctfs/pico22/crypto/pwn/buffer-overflow-0':
-      '/blog/picoctf-2022-buffer-overflow',
-    '/ctfs/pico22/crypto/pwn/buffer-overflow-1':
-      '/blog/picoctf-2022-buffer-overflow',
-    '/ctfs/pico22/crypto/pwn/buffer-overflow-2':
-      '/blog/picoctf-2022-buffer-overflow',
-    '/ctfs/pico22/crypto/pwn/buffer-overflow-3':
-      '/blog/picoctf-2022-buffer-overflow',
-    '/blog/picoctf-2022/buffer-overflow-series':
-      '/blog/picoctf-2022-buffer-overflow',
-    '/ctfs/sekai/compilation': '/blog/sekaictf-2022-forensics-compilation',
-    '/ctfs/shctf/compilation': '/blog/shctf-2022-compilation',
-    '/ctfs/shctf/pwn/guardians-of-the-galaxy': '/blog/shctf-2022-compilation',
-    '/ctfs/shctf/pwn/vader': '/blog/shctf-2022-compilation',
-    '/ctfs/shctf/pwn/warmup-to-the-dark-side': '/blog/shctf-2022-compilation',
-    '/ctfs/shctf/crypto/rahools-challenge': '/blog/shctf-2022-compilation',
-    '/ctfs/actf/algo/gcd-query': '/blog/actf-2023-gcd-query',
-    '/ctfs/idek/osint/nmpz': '/blog/idekctf-2023-nmpz',
-    '/ctfs/mhs/prog/matchmaker': '/blog/mhsctf-2023-matchmaker',
-    '/ctfs/wolv23/osint/wannaflag': '/blog/wolvctf-2023-wannaflag',
-
+    
     // v2.0.0 -> v3.0.0
     '/blog/actf-2023/gcd-query': '/blog/actf-2023-gcd-query',
     '/blog/sekaictf-2023/azusawas-gacha-world': '/blog/azusawas-gacha-world',
